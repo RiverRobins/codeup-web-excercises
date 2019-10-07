c91bca5caa9e6cb67e08c560df5d9d66b517d460
RiverRobins <rivergrahamrobins@gmail.com>
2019-10-07 09:42:58 -0500

    added external_js, inline_js

---
+++ js/external.js
@@ -0,0 +1,15 @@
+//external js exercise
+console.log("Hello from external JavaScript");
+alert("Welcome to my website");
+var favColor = prompt("what is your favorite color?");
+if (favColor === "green") {
+    alert("Great, " + favColor + " is my favorite color too.");
+}
+else {
+    alert("Great, " + favColor + " is a good color(but green is still better).");
+}
+
+//previous exercise
+var a = 1;
+var b = a++;
+var c = ++a;
