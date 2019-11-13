4308c90594b00246b0981b24fe2fbab780ba96ec
RiverRobins <rivergrahamrobins@gmail.com>
2019-11-13 12:30:41 -0600

    sigh

--- js/promises.js
+++ js/promises.js
@@ -1,16 +1,22 @@
-const resolve = new Promise((resolve) => {
-    return resolve();
-});
 const wait = (time) => {
-    setTimeout(() => {
-
-    }, time);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
 };
 
-console.log(wait());
-resolve.then(() => console.log('Revolved'));
-resolve.catch(() => console.log('Error'));
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 
+function ping(user) {
+    const data = fetch(`https://api.github.com/users/:${user}/events/public`, {headers: {'Authorization': 'token ' + gitKey}})
+        .then(response => {response.json()})
+        // .then(users => users.map(user => user.login))
+}
 
-//const data = fetch(url, {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}});
+// /users/:username/events/public
+// https://api.github.com/users/:
+
+console.log(ping("RiverRobins"));
